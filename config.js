bc0b468dd775859f81760cceaa3ce80ae16349c9
Faique Moqeet <faiquemoqeet@gmail.com>
2017-01-26 16:26:00 -0600

    Twitter API functional

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'VN7SJaRmHO8nXxd7Gm21bnK36',
+  consumer_secret:      'oQq1hHVRTns4I8WCyXsdP7aYwVJ6Hxr21l8eWMApLjKW6i0CCT',
+  access_token:         '19051059-rHgjjIgkPIDMcVFvYRiWYQVV3BbGA2Gpci4AOVZXn',
+  access_token_secret:  'x2nqgaCiHXmhbesAJMDiXuz89hfTTXZHZdlK1CalOYHCn',
+}
--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+console.log("The Bot is starting");
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+var T = new Twit(config);
+
+
+T.post('statuses/update', { status: 'Juverias the best!'}, function(err, data, response) {
+  console.log(data)
+})
