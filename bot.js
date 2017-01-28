4fa9f12a722601a948a0269ca42773a1de684f84
Faique Moqeet <faiquemoqeet@gmail.com>
2017-01-28 01:39:07 -0600

    Changed file name of hello.js to bot.js

---
+++ bot.js
@@ -0,0 +1,40 @@
+console.log("The follow Bot is starting");
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+var T = new Twit(config);
+
+
+function tweetIt(text) {
+
+var parameters = { 
+	status: text
+}
+
+function postPost(err, data, response) {
+	if (err) {
+		console.log("You got an error")
+	} else {
+		console.log("It worked!");
+	}
+}
+
+
+T.post('statuses/update', parameters, postPost());
+
+};
+
+
+
+var stream = T.stream('user')
+ 
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+	console.log("Follow Bot Working")
+	var name = eventMsg.source.name; 
+	var screenName = eventMsg.source.screen_name;
+	tweetIt("@" + screenName + " Thanks for following me!");
+}
